3c66beaa460b227e9fd10a79d7bbfa9c5145eb30
Mariam <mari.kifshidze@gmail.com>
2024-01-08 14:13:34 +0400

    Multiprocessing with arguments

--- multiprocessing_intro.py
+++ multiprocessing_intro.py
@@ -1,13 +1,14 @@
 import multiprocessing
+import concurrent.futures
 import time
 
 start = time.perf_counter()
 
 
-def do_something():
-    print('Sleeping 1 second...')
-    time.sleep(1)
-    print('Done Sleeping...')
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return 'Done Sleeping...'
 
 
 # p1 = multiprocessing.Process(target=do_something)
@@ -19,15 +20,22 @@ def do_something():
 # p1.join()
 # p2.join()
 
-processes = []
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    results = [executor.submit(do_something, 1) for _ in range(10)]
 
-for _ in range(100):
-    p = multiprocessing.Process(target=do_something)
-    p.start()
-    processes.append(p)
+    for future in concurrent.futures.as_completed(results):
+        print(future.result())
 
-for process in processes:
-    process.join()
+
+# processes = []
+#
+# for _ in range(100):
+#     p = multiprocessing.Process(target=do_something, args=[1.5])
+#     p.start()
+#     processes.append(p)
+#
+# for process in processes:
+#     process.join()
 
 finish = time.perf_counter()
 
